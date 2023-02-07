4eddbc0927a67fddf2014e406be4781688df5f81
odron <oleg.dronov98@gmail.com>
2023-02-07 12:03:04 +0300

    Добавлена симуляция

--- src/test/scala/perf/TestSimulation.scala
+++ src/test/scala/perf/TestSimulation.scala
@@ -14,7 +14,9 @@ class TestSimulation extends Simulation {
     .disableFollowRedirect
 
   setUp(
-    CommonScenario().inject(atOnceUsers(1)
+    CommonScenario().inject(
+      rampUsersPerSec(0) to 10 during (200),
+      constantUsersPerSec(10) during(1000)
     )
   ).protocols(httpProtocol)
 }
